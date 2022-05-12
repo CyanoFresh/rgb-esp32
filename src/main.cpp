4190465d0c4c1b218190f7a92b76f09f33a7ac3f
CyanoFresh <cyanofresh@gmail.com>
2022-05-12 23:25:47 +0300

    added ledc

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,24 @@
 #include <BLE2902.h>
 #include <Ticker.h>
 
+#define RED_PIN 16
+#define GREEN_PIN 17
+#define BLUE_PIN 19
+#define VOLTAGE_PIN 36
+
 #define DEVICE_NAME "Calm Flower"
 
 #define MODE_SERVICE "d6694b21-880d-4b4a-adae-256cc1f01e7b"
 #define MODE_CHARACTERISTIC "20103538-ff6b-4c7f-9aba-36a32be2c7c2"
 #define COLOR1_CHARACTERISTIC "5903b942-0ce7-42c2-a29f-ff434521fbe2"
-#define COLOR2_CHARACTERISTIC "f42275ed-b762-4e9d-b0c4-2e01d37ae2fd"
+//#define COLOR2_CHARACTERISTIC "f42275ed-b762-4e9d-b0c4-2e01d37ae2fd"
 
 #define BATTERY_SERVICE (uint16_t) 0x180F
 #define BATTERY_CHARACTERISTIC (uint16_t) 0x2A19
+
+#define RED_CHANNEL 0
+#define GREEN_CHANNEL 1
+#define BLUE_CHANNEL 2
 
 typedef enum {
     STATIC,
@@ -24,17 +33,29 @@ BLEServer *server = nullptr;
 BLECharacteristic *batteryCharacteristic = nullptr;
 BLECharacteristic *modeCharacteristic = nullptr;
 BLECharacteristic *color1Characteristic = nullptr;
-uint8_t connectedCount = 0;
 
 Ticker batteryTicker;
 
 void readBattery() {
-    auto batteryLevel = (uint8_t) (analogRead(4) * 100 / 4095);
+    auto batteryLevel = (uint8_t) (analogRead(VOLTAGE_PIN) * 100 / 4095);
     batteryCharacteristic->setValue(&batteryLevel, 1);
     batteryCharacteristic->notify();
 }
 
+void setupLed() {
+    ledcSetup(RED_CHANNEL, 10000, 10);
+    ledcSetup(GREEN_CHANNEL, 10000, 10);
+    ledcSetup(BLUE_CHANNEL, 10000, 10);
+
+    ledcAttachPin(RED_PIN, RED_CHANNEL);
+    ledcAttachPin(GREEN_PIN, GREEN_CHANNEL);
+    ledcAttachPin(BLUE_PIN, BLUE_CHANNEL);
+}
+
 class MyServerCallbacks : public BLEServerCallbacks {
+protected:
+    uint8_t connectedCount = 0;
+public:
     void onConnect(BLEServer *pServer) override {
         connectedCount++;
 
@@ -71,14 +92,18 @@ public:
     void onWrite(BLECharacteristic *pCharacteristic) override {
         auto *data = (uint16_t *) pCharacteristic->getData();
 
+        pCharacteristic->notify();
+
+        ledcWrite(RED_CHANNEL, data[0]);
+        ledcWrite(GREEN_CHANNEL, data[1]);
+        ledcWrite(BLUE_CHANNEL, data[2]);
+
         Serial.print("Color1 changed: ");
         Serial.print(data[0]);
         Serial.print(" ");
         Serial.print(data[1]);
         Serial.print(" ");
         Serial.println(data[2]);
-
-        pCharacteristic->notify();
     }
 };
 
@@ -86,6 +111,7 @@ void setup() {
     Serial.begin(115200);
 
     BLEDevice::init(DEVICE_NAME);
+    // TODO: security
 //    BLEDevice::setEncryptionLevel(ESP_BLE_SEC_ENCRYPT);
 //    BLEDevice::setSecurityCallbacks();
 //    BLESecurity *pSecurity = new BLESecurity();
@@ -143,6 +169,8 @@ void setup() {
 
     batteryTicker.attach(60, readBattery);
     readBattery();
+
+    setupLed();
 
     Serial.println("Started");
 }

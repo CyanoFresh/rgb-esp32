5d7d325e686d337c169cb30cab9a715717f6b374
CyanoFresh <cyanofresh@gmail.com>
2022-05-15 22:44:13 +0300

    Added turn on characteristic

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #define MODE_SERVICE "d6694b21-880d-4b4a-adae-256cc1f01e7b"
 #define MODE_CHARACTERISTIC "20103538-ff6b-4c7f-9aba-36a32be2c7c2"
 #define COLOR1_CHARACTERISTIC "5903b942-0ce7-42c2-a29f-ff434521fbe2"
+#define TURN_ON_CHARACTERISTIC "c9af1949-4275-46ec-9d63-f01fe45e9477"
 //#define COLOR2_CHARACTERISTIC "f42275ed-b762-4e9d-b0c4-2e01d37ae2fd"
 
 #define BATTERY_SERVICE (uint16_t) 0x180F
@@ -33,8 +34,15 @@ BLEServer *server = nullptr;
 BLECharacteristic *batteryCharacteristic = nullptr;
 BLECharacteristic *modeCharacteristic = nullptr;
 BLECharacteristic *color1Characteristic = nullptr;
+BLECharacteristic *turnOnCharacteristic = nullptr;
 
 Ticker batteryTicker;
+
+uint8_t mode = STATIC;
+uint16_t red = 0;
+uint16_t green = 0;
+uint16_t blue = 1023;
+uint8_t turnOn = 1;
 
 void readBattery() {
     auto batteryLevel = (uint8_t) (analogRead(VOLTAGE_PIN) * 100 / 4095);
@@ -78,12 +86,14 @@ public:
 class ModeCharacteristicCallbacks : public BLECharacteristicCallbacks {
 public:
     void onWrite(BLECharacteristic *pCharacteristic) override {
-        uint8_t *mode = pCharacteristic->getData();
-
-        Serial.print("Mode changed: ");
-        Serial.println(*mode);
+        auto *data = pCharacteristic->getData();
 
         pCharacteristic->notify();
+
+        mode = *data;
+
+        Serial.print("Mode changed: ");
+        Serial.println(mode);
     }
 };
 
@@ -94,16 +104,45 @@ public:
 
         pCharacteristic->notify();
 
-        ledcWrite(RED_CHANNEL, data[0]);
-        ledcWrite(GREEN_CHANNEL, data[1]);
-        ledcWrite(BLUE_CHANNEL, data[2]);
+        red = data[0];
+        green = data[1];
+        blue = data[2];
+
+        ledcWrite(RED_CHANNEL, red);
+        ledcWrite(GREEN_CHANNEL, green);
+        ledcWrite(BLUE_CHANNEL, blue);
 
         Serial.print("Color1 changed: ");
-        Serial.print(data[0]);
+        Serial.print(red);
         Serial.print(" ");
-        Serial.print(data[1]);
+        Serial.print(green);
         Serial.print(" ");
-        Serial.println(data[2]);
+        Serial.println(blue);
+    }
+};
+
+class TurnOnCharacteristicCallbacks : public BLECharacteristicCallbacks {
+public:
+    void onWrite(BLECharacteristic *pCharacteristic) override {
+        auto *data = pCharacteristic->getData();
+
+        pCharacteristic->notify();
+
+        turnOn = *data;
+
+        if (turnOn == 1) {
+            ledcWrite(RED_CHANNEL, red);
+            ledcWrite(GREEN_CHANNEL, green);
+            ledcWrite(BLUE_CHANNEL, blue);
+
+            Serial.println("Turned on");
+        } else {
+            ledcWrite(RED_CHANNEL, 0);
+            ledcWrite(GREEN_CHANNEL, 0);
+            ledcWrite(BLUE_CHANNEL, 0);
+
+            Serial.println("Turned off");
+        }
     }
 };
 
@@ -139,8 +178,7 @@ void setup() {
             BLECharacteristic::PROPERTY_NOTIFY
     );
     modeCharacteristic->addDescriptor(new BLE2902());
-    uint8_t defaultMode = STATIC;
-    modeCharacteristic->setValue(&defaultMode, 1);
+    modeCharacteristic->setValue(&mode, 1);
     modeCharacteristic->setCallbacks(new ModeCharacteristicCallbacks());
 
     color1Characteristic = modeService->createCharacteristic(
@@ -152,18 +190,29 @@ void setup() {
     );
     color1Characteristic->addDescriptor(new BLE2902());
 
-    uint16_t value[] = {0, 1023, 0};
+    uint16_t value[] = {red, green, blue};
     color1Characteristic->setValue((uint8_t *) value, 6);
 
     color1Characteristic->setCallbacks(new Color1CharacteristicCallbacks());
+
+    turnOnCharacteristic = modeService->createCharacteristic(
+            TURN_ON_CHARACTERISTIC,
+            BLECharacteristic::PROPERTY_READ |
+            BLECharacteristic::PROPERTY_WRITE |
+            BLECharacteristic::PROPERTY_WRITE_NR |
+            BLECharacteristic::PROPERTY_NOTIFY
+    );
+    turnOnCharacteristic->addDescriptor(new BLE2902());
+    turnOnCharacteristic->setValue(&turnOn, 1);
+    turnOnCharacteristic->setCallbacks(new TurnOnCharacteristicCallbacks());
 
     modeService->start();
 
     server->getAdvertising()->addServiceUUID(MODE_SERVICE);
     // TODO: https://specificationrefs.bluetooth.com/assigned-values/Appearance%20Values.pdf
-//    server->getAdvertising()->setAppearance();
+    server->getAdvertising()->setAppearance(0x03C0);
     server->getAdvertising()->setScanResponse(true);
-    server->getAdvertising()->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+    server->getAdvertising()->setMinPreferred(0x06);  // functions that help with iPhone connections issue ?
     server->getAdvertising()->setMinPreferred(0x12);
     server->getAdvertising()->start();
 

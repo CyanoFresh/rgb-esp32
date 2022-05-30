f63c5a6109608c5405cccf580f8a49997da5a550
CyanoFresh <cyanofresh@gmail.com>
2022-05-30 20:35:09 +0300

    moved to config.h; fixed battery readings

--- src/main.cpp
+++ src/main.cpp
@@ -6,43 +6,7 @@
 #include <WiFi.h>
 #include <ArduinoOTA.h>
 #include <Preferences.h>
-
-#define RED_PIN 16
-#define GREEN_PIN 17
-#define BLUE_PIN 19
-#define VOLTAGE_PIN 36
-
-#define DEVICE_NAME "Legendary Invention"
-
-#define MODE_SERVICE "d6694b21-880d-4b4a-adae-256cc1f01e7b"
-#define MODE_CHARACTERISTIC "20103538-ff6b-4c7f-9aba-36a32be2c7c2"
-#define COLOR1_CHARACTERISTIC "5903b942-0ce7-42c2-a29f-ff434521fbe2"
-#define TURN_ON_CHARACTERISTIC "c9af1949-4275-46ec-9d63-f01fe45e9477"
-#define SPEED_CHARACTERISTIC "74d51f60-ed42-4f82-b189-0fab7ffa7cd9"
-//#define COLOR2_CHARACTERISTIC "f42275ed-b762-4e9d-b0c4-2e01d37ae2fd"
-
-#define BATTERY_SERVICE (uint16_t) 0x180F
-#define BATTERY_CHARACTERISTIC (uint16_t) 0x2A19
-
-#define OTA_SERVICE "e3414eb0-bfa8-41f6-a3ee-db0b722e5807"
-#define OTA_CHARACTERISTIC "1e2b6f32-a786-441c-acc9-6e2e5637cfb3"
-
-#define RED_CHANNEL 0
-#define GREEN_CHANNEL 1
-#define BLUE_CHANNEL 2
-
-#define MAX_COLOR_VALUE 4095
-
-#define BATTERY_LOW 3000
-#define BATTERY_HIGH 3900
-
-#define BATTERY_LOW 3000
-#define BATTERY_HIGH 3900
-
-#define BATTERY_LOW_TURNED_ON 2800
-#define BATTERY_HIGH_TURNED_ON 3700
-
-#define OTA_PASSWORD "12345678"
+#include <config.h>
 
 typedef enum {
     STATIC,
@@ -95,9 +59,23 @@ void savePreferences() {
     Serial.println("Preferences saved");
 }
 
+uint8_t reading = 0;
+uint sum = 0;
+
 void readBattery() {
-    // TODO: jumping values
     uint16_t value = analogRead(VOLTAGE_PIN);
+
+    sum += value;
+    reading++;
+
+    if (reading >= 10) {
+        value = sum / reading;
+
+        reading = 0;
+        sum = 0;
+    } else {
+        return;
+    }
 
     uint16_t low = BATTERY_LOW_TURNED_ON;
     uint16_t high = BATTERY_HIGH_TURNED_ON;
@@ -119,7 +97,7 @@ void readBattery() {
 
     batteryCharacteristic->setValue(&percent, 1);
     batteryCharacteristic->notify();
-    
+
     Serial.print("Battery level: ");
     Serial.print(batteryLevel);
     Serial.print("%, ");
@@ -129,8 +107,7 @@ void readBattery() {
 void setupBattery() {
     pinMode(VOLTAGE_PIN, INPUT);
 
-    // TODO: adjust timings
-    batteryTicker.attach(3, readBattery);
+    batteryTicker.attach_ms(2000, readBattery);
     readBattery();
 }
 
